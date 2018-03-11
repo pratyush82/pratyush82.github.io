c4ab2be7f78eb59c7f74ba3c6bc3b5fb72dfed66
Pratyush Karmakar <pratyushk190398@gmail.com>
2018-03-11 11:26:22 +0530

    webpages added

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("hom1").href = "home.html";
+document.getElementById("hom").href = "home.html";
+document.getElementById("elyf").href = "earlylife.html";
+document.getElementById("awa").href = "awards.html";
+document.getElementById("phot").href = "qua.html";
+document.getElementById("wave").href = "wpd.html";
+document.getElementById("relat").href = "rel.html";
+document.getElementById("bose").href = "bes.html";
